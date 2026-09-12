2adab481f110bc5c980840a8f88f8c89e52c93d3
camila torres <camilitatorres2006@gmail.com>
2026-09-11 20:54:15 -0500

    Refactorización en la validación de contraseña
    Se añade @Pattern con expresión regular para la validación de las condiciones de la contraseña (CA4) y se elimina lógica de validación del RegisterService

--- backend/src/main/java/com/crowdfunding/backend/services/RegisterService.java
+++ backend/src/main/java/com/crowdfunding/backend/services/RegisterService.java
@@ -1,6 +1,5 @@
 package com.crowdfunding.backend.services;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,8 +10,6 @@ import com.crowdfunding.backend.exception.DuplicatedEmailException;
 import com.crowdfunding.backend.persistence.UserRepository;
 import com.crowdfunding.backend.register.RegisterRequest;
 import com.crowdfunding.backend.register.RegisterResponse;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class RegisterService {
@@ -24,16 +21,7 @@ public class RegisterService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*\\d)(?=.*[A-Z]).{8,}$");
-
     public RegisterResponse register(RegisterRequest request) {
-
-        Matcher matcher = PASSWORD_PATTERN.matcher(request.password());
-
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException(
-                    "La contraseña debe tener mínimo 8 caracteres, al menos un número y al menos una letra mayúscula.");
-        }
 
         Optional<User> result = repository.findByEmail(request.email());
 
