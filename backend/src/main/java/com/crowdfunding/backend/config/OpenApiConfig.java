378a35c45954fafeec457452dc4b94b7899c49d7
camila torres <camilitatorres2006@gmail.com>
2026-09-13 13:22:56 -0500

    Agrega OpenAPI config con JWT bearer auth
    Añade configuración para Swagger/OpenAPI que define metadatos de API y seguridad global `bearerAuth`. Permite soporte para JWT bearer token en la documentación de API.

---
+++ backend/src/main/java/com/crowdfunding/backend/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.crowdfunding.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "CrowdfundinAPI", version = "1.0"), 
+    security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(
+    name = "bearerAuth",
+    description = "Ingresa el token JWT",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {}
